d61e2f66fc0696808cb36e480d626f231c679acd
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-03-09 19:06:33 +0530

    Changed which-key config to add new key mappings

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,32 +1,56 @@
 local status_ok, wk = pcall(require, "which-key")
 if not status_ok then
-	error("Which-Key Error")
-	return
+  error("Which-Key Error")
+  return
 end
 
 -- Setup `which-key` with configurations
 wk.setup({
-	plugins = {
-		marks = true,
-		registers = true,
-		spelling = { enabled = true, suggestions = 20 },
-	},
-	icons = {
-		breadcrumb = "»",
-		separator = "",
-		group = " ",
-	},
-	replace = {
-		["<leader>"] = "SPC",
-	},
+  plugins = {
+    marks = true,
+    registers = true,
+    spelling = { enabled = true, suggestions = 20 },
+  },
+  icons = {
+    breadcrumb = "»",
+    separator = "",
+    group = " ",
+  },
+  replace = {
+    ["<leader>"] = "SPC",
+  },
 })
 
 -- Add key mappings with groups
 wk.add({
-	{ "<leader>b", group = "Buffer" },
-	{ "<leader>bd", group = "Delete" },
-	{ "<leader>bp", group = "Pinned" },
-	{ "<leader>f", group = "Find (Telescope)" },
-	{ "<leader>g", group = "Git" },
-	{ "<leader>x", group = "Trouble Diagnostics" },
+  -- Lazy
+  { "<leader>l",       group = "[L]azy" },
+
+  -- Yank
+  { "<leader>y",       group = "[Y]ank To System Clipboard" },
+
+  -- Buffers
+  { "<leader>b",       group = "[B]uffer" },
+  { "<leader>bd",      group = "[D]elete" },
+  { "<leader>bp",      group = "[P]in" },
+  { "<leader>bs",      group = "[S]how" },
+
+  -- Telescope Finder
+  { "<leader>f",       group = "[F]ind" },
+  { "<leader>ft",      group = "[T]odo" },
+  { "<leader>/",       group = "Find Text In CWD" },
+  { "<leader><space>", group = "Find Files" },
+
+  -- Git
+  { "<leader>g",       group = "[G]it" },
+  { "<leader>gb",      group = "[B]ranches" },
+
+  -- Mason
+  { "<leader>m",       group = "[M]ason" },
+
+  -- Undotree
+  { "<leader>u",       group = "[U]ndotree" },
+
+  -- Quit
+  { "<leader>q",       group = "[Q]uit" },
 })
