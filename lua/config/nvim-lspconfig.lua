efac6bca76d34cdb0f4fe19c62b524c8e24b86eb
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-04-05 18:56:44 +0530

    Changed diagnostics for removing deprecating error

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -14,11 +14,16 @@ end
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change The Diagnostic Symbols In The Gutter
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = "󰠠 ",
+    },
+  },
+})
 
 -- Golang
 lspconfig["gopls"].setup({
